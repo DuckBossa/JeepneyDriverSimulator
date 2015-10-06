2a91ca160f42238ec978250d54c73539d46e3669
John Joseph Ugalino <jjrugalino@Arghs-MacBook-Air.local>
2015-10-06 15:23:14 +0800

    mapgen

--- Jeepney Driver Simulator/Assets/Scripts/MapGenerator.cs
+++ Jeepney Driver Simulator/Assets/Scripts/MapGenerator.cs
@@ -1,29 +1,50 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-
 public class MapGenerator : MonoBehaviour {
 
 	public Texture2D mapImage;
-	public GameObject road;
-	public GameObject sidewalk;
+	public GameObject tile;
 	public int tileSize;
 
-	public Transform root;
+	private Transform root;
 
 	public void GenerateMap() {
-		byte[] temp = mapImage.GetRawTextureData ();
-		root = GetComponent<Transform>;
+//		Color[] temp = mapImage.GetPixels(0,0,mapImage.width,mapImage.height );
+		Color[] temp = mapImage.GetPixels ();
+//		foreach (Color v in temp) {
+//			Debug.Log (v);
+//		}
+
+		root = gameObject.transform;
 		short count = 0;
 		int length = Mathf.RoundToInt(Mathf.Sqrt(temp.GetLength(0)));
-		Vector3 start = new Vector3 (-length*tileSize/2, 0, length*tileSize/2);
-		foreach(byte value in temp){
-			GameObject prefab = null;
-			if(value == 255) prefab = road;
-			else prefab = sidewalk;
-			GameObject node = Instantiate(prefab, start + new Vector3((count++ % length)*tileSize, 0, (count / length)*tileSize), Quaternion.identity);
-			Transform x = node.GetComponent<Transform>;
+		Vector3 start = new Vector3 (-length*tileSize/2, 0, -length*tileSize/2);
+
+
+//		for (int w = 0; w < mapImage.width; w++) {
+//			for(int h = 0; h < mapImage.height; h++){
+//				mapImage.get
+//			}
+//		}
+//
+//
+//
+		foreach(Color value in temp){
+			GameObject node = Instantiate(tile, start + new Vector3((count % length)*tileSize, 0, (count++ / length)*tileSize), Quaternion.identity) as GameObject;
+
+			if(value.r == 0f){
+				node.GetComponent<Tile>().SwitchTileType(Tile.TileType.Road);
+	
+			}
+			else{
+				node.GetComponent<Tile>().SwitchTileType(Tile.TileType.Pathway);
+			}
+
+			Transform x = node.transform;
 			x.SetParent(root);
 		}
+//		Debug.Log (count);
+//		Debug.Log (temp.GetLength(0));
 	}
 }
---
+++ Jeepney Driver Simulator/Assets/Scripts/Tile.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tile : MonoBehaviour {
+
+	public enum TileType{
+		Road, Pathway
+	}
+
+	public TileType tt;
+
+	public void Start(){
+		SwitchTileType (TileType.Pathway);
+	}
+
+	public void SwitchTileType(TileType x){
+		tt = x;
+		switch(tt){
+		case TileType.Pathway:
+//			gameObject.GetComponent<MeshRenderer>().material.color = Color.blue;
+			break;
+		case TileType.Road:
+//			gameObject.GetComponent<MeshRenderer>().material.color = Color.red;
+			break;
+		}
+		gameObject.layer = LayerMask.NameToLayer(tt.ToString());
+	}
+
+}
