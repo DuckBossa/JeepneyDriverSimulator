96353ada681ad5e6488a38ecbcaf40692181cd8f
John Joseph Ugalino <jjrugalino@Arghs-MacBook-Air.local>
2015-10-07 19:17:28 +0800

    made AI controller

---
+++ Jeepney Driver Simulator/Assets/Scripts/PedestrianController.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+namespace UnitySteer.Behaviors{
+
+	public class PedestrianController : MonoBehaviour {
+		
+		public SteerForWander wanderScript;
+		public SteerForSphericalObstacles avoidScript;
+		public  ridingScript; // check unity steer component
+		
+		
+		
+		public void Start(){
+			wanderScript = GetComponent<SteerForWander> ();
+			avoidScript = GetComponent<SteerForSphericalObstacles> ();
+
+		}
+		
+		
+		public enum PedestrianState{
+			Wander,
+			Riding,
+			Looking
+		}
+		
+		public PedestrianState currState;
+		
+		
+		private void changeState(PedestrianState x){
+			currState = x;
+			wanderScript.gameObject.SetActive (false);
+			ridingScript.gameObject.SetActive (false);
+			switch (currState) {
+			case PedestrianState.Looking:
+				ridingScript.gameObject.SetActive(true);
+				break;
+			case PedestrianState.Wander:
+				wanderScript.gameObject.SetActive(true);
+				break;
+			case PedestrianState.Riding:
+				Debug.Log("STOP");
+				break;
+				
+			}
+		}
+		
+	}
+
+
+
+}
