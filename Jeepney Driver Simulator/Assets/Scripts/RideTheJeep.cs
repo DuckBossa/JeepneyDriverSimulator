4575afc1b933dc8bb47a3bc7ab00ab5faa4e62f6
Raphael <DuckBossa@gmail.com>
2015-11-29 23:51:05 +0800

    beep beep scripts

---
+++ Jeepney Driver Simulator/Assets/Scripts/RideTheJeep.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RideTheJeep : MonoBehaviour {
+	public	RidingScript rs;
+
+	public void OnEnable(){
+		Board();
+	}
+	
+	private void Board(){
+		Debug.Log("BORD MEEEEEEEEEEEEEEEEEEEEE");
+		rs.AddPassenger(gameObject);
+
+	}
+}
